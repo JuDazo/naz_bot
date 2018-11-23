773f44496ce364809e421da4404cf7d38df4a68c
Assfalg Daniel <Assfalg@Blum-Novotest.com>
2018-11-23 11:14:58 +0100

    init commit

---
+++ into_GoogleTab.py
@@ -0,0 +1,89 @@
+import gspread
+import time
+from oauth2client.service_account import ServiceAccountCredentials
+
+wochen = {
+    "Mon": "Mo",
+    "Tue": "Di",
+    "Wed": "Mi",
+    "Thu": "Do",
+    "Fri": "Fr",
+    "Sat": "Sa",
+    "Sun": "So"
+}
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+
+wochentag = time.strftime("%a")
+wochentag = wochen[wochentag]
+w_string = wochentag + ".\xa0" + time.strftime("%d.%m")
+
+def in_to_database(args):
+    spende = {"user": None, "type": None, "value": None, "negativ" : False}
+    for a in args:
+        if a == "-":
+            spende["negativ"] = True
+        elif a.isdigit() or a.lstrip("-").isdigit() or a.lstrip("+").isdigit():
+            spende["value"] = int(a)
+        elif str(a) == "K" or str(a) == "k":
+            spende["type"] = "Kristal"
+        elif str(a) == "S" or str(a) == "s":
+            spende["type"] = "Siegel"
+        else:
+            spende["user"] = str(a).lower()
+
+    for e in spende:
+        if spende[e] == None:
+            return -3
+
+    sheett = client.open('Test')
+    if spende["type"] == "Kristal":
+        sheet = sheett.get_worksheet(0)
+        try:
+            day = sheet.row_values(1)
+            names = sheet.col_values(5)
+        except Exception as e:
+            print(e + "Problem with day & names")
+            return -1
+
+    elif spende["type"] == "Siegel":
+        sheet = sheett.get_worksheet(1)
+        try:
+            day = sheet.row_values(2)
+            names = sheet.col_values(5)
+        except Exception as e:
+            print(e + "Problem with day & names")
+            return -1
+    column = 0
+    day_ex = False
+    for d in day:
+        column+=1
+        if d == w_string:
+            day_ex = True
+            break
+    row=0
+    user_ex = False
+    for n in names:
+        row+=1
+        if n.lower() == spende["user"]:
+            user_ex = True
+            break
+
+    if not user_ex:
+        return -2
+    elif not day_ex:
+        return -1
+
+    curr = sheet.cell(row,column).value.replace(".", "")
+    if spende["negativ"]:
+        new = "=" + curr + "-" + str(spende["value"])
+    else:
+        new = "="+curr+"+"+str(spende["value"])
+    sheet.update_cell(row,column,new)
+
+    val = sheet.cell(row, 4).value
+
+
+    return val
---
+++ main.py
@@ -0,0 +1,83 @@
+import asyncio
+import discord
+import SECRET, STATICS
+import into_GoogleTab
+
+client = discord.Client()
+
+@client.event
+@asyncio.coroutine
+def in_to_database(args, client, ret, message):
+    spende = {"user": None, "type": None, "value": None, "negativ" : None}
+    for a in args:
+        if a == "-":
+            spende["negativ"] = True
+        elif a.isdigit() or a.lstrip("-").isdigit() or a.lstrip("+").isdigit():
+            spende["value"] = int(a)
+        elif str(a) == "K" or str(a) == "k":
+            spende["type"] = "Kristal"
+        elif str(a) == "S" or str(a) == "s":
+            spende["type"] = "Siegel"
+        else:
+            spende["user"] = str(a).lower()
+
+    if spende["negativ"]:
+        spende["value"] = "-"+str(spende["value"])
+
+    if ret == -1:
+        yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
+        'Die Spende konnte nicht eingetragen werden bitte melde dich bei JuDazo das du den Fehler mit den Code -1 gefunden hast danke')))
+    elif ret == -2:
+        yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
+        'Der angegebene Username %s konnte nicht in der Tabelle gefunden werden \n' % (spende["user"]))))
+    elif ret == -3:
+        yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
+        'Bitte versuche es noch ein mal und gib diesmla alle wichtigen informationen an. \n Denk daran das Format ist'
+        ' "Spende Menge Art Username" \n Art = S für Siegel \n Art = K für Kristalle\n Die Menge der Kristalle oder Sigel bitte nicht mit "." oder "," trennen' )))
+    else:
+        yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
+                'Spende Eingetragen:\n%s hat %s %s gespendet \nDanke das sie das Automatische spendensystem von Dän '
+                'verwenden:)' % (spende["user"], spende["value"], spende["type"]))))
+        ret=ret.replace(".", "")
+        if int(ret) < 0 :
+            yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=(
+                'Du musst noch : %s %s Spenden)' % (ret.replace("-", ""),spende["type"]))))
+        else:
+            yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=(
+                    'Du hast ein guthaben von: %s %s )' % (ret.replace("+", ""), spende["type"]))))
+@client.event
+@asyncio.coroutine
+def on_ready():
+    print("Bot is logged in seccessfully. Running on servers:\n")
+    for s in client.servers:
+        print(s)
+        print("- %s (%s)"% (s.name , s.id))
+    yield from client.change_presence(game=discord.Game(name="Ich bin das spenden luder"))
+
+
+@client.event
+@asyncio.coroutine
+def on_message(message):
+    if str(message.author) != STATICS.BOT:
+        if message.attachments == []:
+            if str(message.channel) == STATICS.CHANNEL:
+                if message.content.lower().startswith(STATICS.PREFIX.lower()):
+                    args = message.content.split(" ")[1:]
+
+                    ret = into_GoogleTab.in_to_database(args)
+                    yield from in_to_database(args, client, ret,message)
+                else:
+                    #yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=
+                     #  ('Das angegebene Format der Spende war nicht Richtig bitte halte dich an das Format:'
+                      #   '\n"Spende Menge Art Username" \n Art = S für Siegel \n Art = K für Kristalle \nDanke :)')))
+                    yield from client.send_message(message.channel,
+                                                   embed=discord.Embed(color=discord.Color.red(), description=
+                                                   (
+                                                       'Das angegebene Format der Spende war nicht Richtig du hast das Key Wort "Spende" vergessen\n bitte halte dich an das Format:'
+                                                       '\n"Spende Menge Art Username" \n Art = S für Siegel \n Art = K für Kristalle \nDanke :)')))
+
+
+
+
+
+client.run(SECRET.TOKEN)
