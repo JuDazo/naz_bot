1f76bb7045c823a59409041686d5bb625a6f94b5
Dan <d.assfalg@web.de>
2018-11-28 10:55:09 +0100

    code clean fehler genauer beschrieben und kleine bug fixes

--- into_GoogleTab.py
+++ into_GoogleTab.py
@@ -24,7 +24,6 @@ def into_google_init():
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
     client = gspread.authorize(creds)
 
-
 def get_Data(type):
     global scope
     global creds
@@ -55,6 +54,18 @@ def get_Data(type):
             if mdata["properties"]["title"] == "vgl-SzuE":
                 sheet = sheett.get_worksheet(mdata["properties"]["index"])
                 return sheet
+    else:
+        print("daten blatt nicht vorhabend")
+        return -1
+
+def get_day_name(sheet,day_row):
+        try:
+                day = sheet.row_values(day_row)
+                names = sheet.col_values(5)
+                return [day,names]
+        except Exception as e:
+                print(e + "Problem with tabel")
+                return [-1,-1]
 
 def in_to_database(args):
     wochentag = time.strftime("%a")
@@ -78,8 +89,9 @@ def in_to_database(args):
     for e in spende:
         if spende[e] == None:
             return -3
-
     sheet = get_Data(spende["type"])
+    if sheet == -1:
+        return -1
     try:
             day = sheet.row_values(day_row)
             names = sheet.col_values(5)
@@ -107,7 +119,7 @@ def in_to_database(args):
         return -2
     elif not day_ex:
         print("Day is "+str(w_string))
-        return -1
+        return -4
 
     curr = sheet.cell(row,column).value.replace(".", "")
     if spende["negativ"]:
@@ -119,7 +131,6 @@ def in_to_database(args):
     val = sheet.cell(row, 4).value
     return val
 
-
 def kontostand(user):
     global scope
     global creds
@@ -128,8 +139,12 @@ def kontostand(user):
         user = user[0].lower()
         val = []
         sheets = [get_Data("Kristal"),get_Data("Siegel")]
+        if sheets[0] ==-1 or sheets[1] ==-1:
+            return -1
         for s in sheets:
-            names = s.col_values(5)
+            names = get_day_name(s,0)[1]
+            if names == -1:
+                return -1
             user_ex = False
             row = 0
             for n in names:
@@ -152,12 +167,13 @@ def oeffnen(args):
         w_string = wochentag + ".\xa0" + time.strftime("%d.%m")
         user =  args[0]
         sheet = get_Data("ein")
-        try:
-                day = sheet.row_values(1)
-                names = sheet.col_values(1)
-        except Exception as e:
-                print(e + "Problem with day & names")
+        if sheet ==-1:
+            return -1
+        day = get_day_name(sheet,1)[0]
+        names = get_day_name(sheet,1)[1]
+        if day == -1 or names == -1:
                 return -1
+
         column = 0
         day_ex = False
         for d in day:
@@ -165,12 +181,11 @@ def oeffnen(args):
             if d == w_string:
                 day_ex = True
                 break
-
         row = 0
         user_ex = False
         for n in names:
             row +=1
-            if n == user:
+            if n.lower() == user.lower():
                 user_ex = True
                 break
         if not user_ex:
--- main.py
+++ main.py
@@ -7,42 +7,54 @@ client = discord.Client()
 
 @client.event
 @asyncio.coroutine
-def in_to_database(args, client, ret, message):
-    spende = {"user": None, "type": None, "value": None, "negativ" : None}
-    for a in args:
-        if a == "-":
-            spende["negativ"] = True
-        elif a.isdigit() or a.lstrip("-").isdigit() or a.lstrip("+").isdigit():
-            spende["value"] = int(a)
-        elif str(a) == "K" or str(a) == "k":
-            spende["type"] = "Kristal"
-        elif str(a) == "S" or str(a) == "s":
-            spende["type"] = "Siegel"
-        else:
-            spende["user"] = str(a).lower()
+def ret_val(args, client, ret, message):
+        spende = {"user": None, "type": None, "value": None, "negativ" : None}
+        for a in args:
+            if a == "-":
+                spende["negativ"] = True
+            elif a.isdigit() or a.lstrip("-").isdigit() or a.lstrip("+").isdigit():
+                spende["value"] = int(a)
+            elif str(a) == "K" or str(a) == "k":
+                spende["type"] = "Kristal"
+            elif str(a) == "S" or str(a) == "s":
+                spende["type"] = "Siegel"
+            else:
+                spende["user"] = str(a).lower()
+        if spende["negativ"]:
+            spende["value"] = "-"+str(spende["value"])
 
-    if spende["negativ"]:
-        spende["value"] = "-"+str(spende["value"])
-    if ret == -1:
-        yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
-        'Die Spende konnte nicht eingetragen werden bitte melde dich bei JuDazo das du den Fehler mit den Code -1 gefunden hast danke')))
-    elif ret == -2:
-        yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
-        'Der angegebene Username %s konnte nicht in der Tabelle gefunden werden \n' % (spende["user"]))))
-    elif ret == -3:
-        yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
-        'Bitte versuche es noch ein mal und gib diesmla alle wichtigen informationen an. \n Denk daran das Format ist'
-        ' "Spende Menge Art Username" \n Art = S für Siegel \n Art = K für Kristalle\n Die Menge der Kristalle oder Sigel bitte nicht mit "." oder "," trennen' )))
-    else:
-        yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
-                'Spende Eingetragen:\n%s hat %s %s gespendet \nDanke das sie das Automatische spendensystem von Dän '
-                'verwenden:)' % (spende["user"], spende["value"], spende["type"]))))
-        ret=ret.replace(".", "")
-        if int(ret) < 0 :
+        if ret == -1:
+            yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
+            'Error 101 Bitte schnellst möglich Judazo per pn bescheid geben danke')))
+
+        elif ret == -2:
+            yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
+            'Der angegebene Username konnte nicht in der Tabelle gefunden werden \n' )))
+        elif ret == -3:
+            yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
+            'Bitte versuche es noch ein mal und gib diesmla alle wichtigen informationen an. \n Denk daran das Format ist'
+            ' "Spende Menge Art Username" \n Art = S für Siegel \n Art = K für Kristalle\n'
+            'Die Menge der Kristalle oder Sigel bitte nicht mit "." oder "," trennen' )))
+        elif ret == -4:
+            yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
+            'Error 404 Bitte schnellst möglich Judazo per pn bescheid geben danke \n' )))
+
+        elif ret == 0:
             yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=(
-                'Du musst noch : %s %s Spenden)' % (ret.replace("-", ""),spende["type"]))))
+            'die geöffnete q wurde eingetragen \n' )))
         else:
-            yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=(
+            if isinstance(ret, (list,)):
+                yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=(
+                "Dein Kontostand beim "+ret[1]+" beträgt "+ret[0]+" und beim "+ret[3]+" beträgt er "+ret[2])))
+            else:
+                ret=ret.replace(".", "")
+                yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
+                    'Spende Eingetragen:\n%s hat %s %s gespendet' % (spende["user"], spende["value"], spende["type"]))))
+                if int(ret) < 0 :
+                    yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=(
+                    'Du musst noch : %s %s Spenden)' % (ret.replace("-", ""),spende["type"]))))
+                else:
+                    yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=(
                     'Du hast ein guthaben von: %s %s )' % (ret.replace("+", ""), spende["type"]))))
 
 @client.event
@@ -55,17 +67,6 @@ def on_ready():
     yield from client.change_presence(game=discord.Game(name="Ich bin das spenden luder"))
 
 
-def kontostand(ret,message):
-    if ret == -2:
-        yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=(
-            'Der angegebene Username konnte nicht in der Tabelle gefunden werden \n' )))
-    elif ret == 0:
-        yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=(
-            'die geöffnete q wurde eingetragen \n' )))
-    else:
-        yield from client.send_message(message.author, embed=discord.Embed(color=discord.Color.red(), description=(
-                "Dein Kontostand beim "+ret[1]+" beträgt "+ret[0]+" und beim "+ret[3]+" beträgt er "+ret[2])))
-
 
 @client.event
 @asyncio.coroutine
@@ -76,15 +77,15 @@ def on_message(message):
                     if message.content.lower().startswith(STATICS.Spende.lower()):
                         args = message.content.split(" ")[1:]
                         ret = into_GoogleTab.in_to_database(args)
-                        yield from in_to_database(args, client, ret,message)
+                        yield from ret_val(args, client, ret,message)
                     elif message.content.lower().startswith(STATICS.Konto.lower()):
                             args = message.content.split(" ")[1:]
                             ret = into_GoogleTab.kontostand(args)
-                            yield from kontostand(ret,message)
+                            yield from ret_val(None,client,ret,message)
                     elif message.content.lower().startswith(STATICS.Eintag):
                             args = message.content.split(" ")[1:]
                             ret = into_GoogleTab.oeffnen(args)
-                            yield from kontostand(ret,message)
+                            yield from ret_val(None,client,ret,message)
                     else:
                         yield from client.send_message(message.channel,
                         embed=discord.Embed(color=discord.Color.red(), description=
